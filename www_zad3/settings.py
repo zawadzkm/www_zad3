4e2f5b70210c23787a40042be012953b66c8e669
zawadzkm <mariusz.zawadzki@roche.com>
2017-05-19 12:25:26 +0200

    allowed_hosts

--- www_zad3/settings.py
+++ www_zad3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wv%(jcz!j(fm00qss!&gv1&u=2#2cvo=15-&xuc5!^3=xa0(u&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
